213d0802188261c6999a572d15fa8f8e60aae816
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-02 03:37:36 +1000

    Nicefy

--- src/plugins/mApi.js
+++ src/plugins/mApi.js
@@ -6,9 +6,12 @@ const mbApi = new MusicBrainzApi({
   appVersion: '0.0.1',
   appContactInfo: 'ryleegeorge1@gmail.com'
 })
-async function mApiPlugin(fastify) {
-  console.log('mApi connected')
+async function mApiPlugin(fastify, opts, done) {
+  const log = fastify.log.child({ module: 'Scanner' })
+  log.debug('Attaching MusicBrain API')
   fastify.decorate('mapi', mbApi)
+  log.info('Attached MusicBrain API')
+  done()
 }
 
 module.exports = fastifyPlugin(mApiPlugin)
