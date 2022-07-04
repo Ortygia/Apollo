13f1a6e13fb63239c58e978c4ecf55c36fca1696
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-05 08:05:07 +1000

    Add/Update models

---
+++ src/models/album.js
@@ -0,0 +1,28 @@
+const { DataTypes, Sequelize } = require('sequelize')
+
+// We export a function that defines the model.
+// This function will automatically receive as parameter the Sequelize connection object.
+module.exports = (sequelize) => {
+  sequelize.define('album', {
+    // The following specification of the 'id' attribute could be omitted
+    // since it is the default.
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    path: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    year: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  })
+}
