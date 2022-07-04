ab98d037df7f6e252d76d8e80b4770a1453abdc0
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-05 08:04:53 +1000

    Change init sytem for scanner

--- src/plugins/musicScanner/process/init.js
+++ src/plugins/musicScanner/process/init.js
@@ -1,25 +1,29 @@
 const MusicScanner = require('./musicScanner')
 const logger = require('pino')({
   name: 'MusicScanner',
-  transport: {
-    target: 'pino-pretty',
-    options: { destination: 1 }
-  }
+  level: 'debug'
+
 })
 const scanner = new MusicScanner('', logger)
 async function run() {
   scanner.initialize()
-  await registerScannerEvents()
-  await registerMasterEvents()
-
   process.send({ t: 'started', message: null })
+  // scanner.startScanning()
+  // process.send({ t: 'started', message: null })
+  registerMasterEvents()
+  registerScannerEvents()
 }
 
 async function registerMasterEvents() {
   process.on('message', message => {
     switch (message.t) {
     case 'startScan':
-      scanner.startScanning()
+
+      scanner.startFullScan()
+      break
+
+    case 'startPartialScan':
+      scanner.startPartialScan()
       break
     }
   })
