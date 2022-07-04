90a3f2498135f9b0b283236eadd0bad50938ff77
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-05 08:04:45 +1000

    Rewrite scanner WIP

--- src/plugins/musicScanner/process/musicScanner.js
+++ src/plugins/musicScanner/process/musicScanner.js
@@ -2,10 +2,12 @@ const Walk = require('@root/walk')
 const mm = require('music-metadata')
 const Sequelize = require('sequelize')
 const EventEmitter = require('node:events')
-
-const path = require('path')
+const SequelizeSimpleCache = require('sequelize-simple-cache')
+const p = require('path')
 const fsp = require('fs/promises')
-const { throws } = require('node:assert')
+const fs = require('fs')
+const directory = require('../../../models/directory')
+const path = require('node:path')
 class MusicScanner extends EventEmitter {
   constructor(mediaFolder, log) {
     super()
@@ -13,20 +15,27 @@ class MusicScanner extends EventEmitter {
     this.mediaFolder = mediaFolder
     this.db = null
     this.log = log
+    this.song = null
+    this.albumsToUpdate = []
   }
 
   async initialize () {
     const sequelize = new Sequelize({
       dialect: 'sqlite',
-      storage: 'apollo.db'
+      storage: 'apollo.db',
+      logging: false
     })
-    const models = [require('../../../models/song')]
+
+    const models = [require('../../../models/song'), require('../../../models/directory'), require('../../../models/album')]
     for (const model of models) {
       model(sequelize, Sequelize.DataTypes)
     }
-    await sequelize.sync({ force: false, logging: false })
+
+    // this.song = cache.init())
+    await sequelize.sync({ force: false, logging: process.env.NODE_ENV === 'production' ? false : console.log })
 
     this.db = sequelize
+    this.db.logging = false
     this.log.info('Started scanner')
   }
 
@@ -38,97 +47,99 @@ class MusicScanner extends EventEmitter {
     this.emit('scanStatus', status)
   }
 
-  async startScanning() {
+  async startFullScan() {
     // if (this.scanning) return false
+    console.time('fullScan')
     this.updateScanStatus(true)
-    /*   await Walk.walk('I:\\Music', walkFunc.bind(this))
+    await Walk.walk('/mnt/g/aa', walkFunc.bind(this))
     // walkFunc must be async, or return a Promise
     async function walkFunc(err, pathname, dirent) {
       if (err) {
-        // throw an error to stop walking
-        // (or return to ignore and keep going)
-        console.log(' Got eeror')
         console.warn('fs stat error for %s: %s', pathname, err.message)
         return null
       }
+      if (dirent.isDirectory()) {
+        if (pathname === '/mnt/g/aa') return true
+        const stats = fs.statSync(pathname)
+        this.updateOrCreate(this.db.models.directory, { path: pathname }, { path: pathname, mtime: stats.mtimeMs })
+      }
       if (dirent.isFile()) {
         if (dirent.name.endsWith('.flac')) {
-          try {
-            const metadata = await mm.parseFile(pathname)
-            if (!await this.db.models.song.findOne({ where: { title: metadata.common.title, album: metadata.common.album } })) {
-              await this.db.models.song.create({
-                title: metadata.common.title,
-                disk: metadata.common.disk.no,
-                album: metadata.common.album,
-                artist: metadata.common.artist,
-                track: metadata.common.track.no,
-                codec: metadata.format.codec,
-                sampleRate: metadata.format.sampleRate,
-                bitsPerSample: metadata.format.bitsPerSample,
-                year: metadata.common.year,
-                label: metadata.common.label,
-                musicBrainzRecordingId: metadata.common.musicbrainz_recordingid,
-                musicBrainzArtistId: metadata.common.musicbrainz_artistid,
-                musicBrainzTrackId: metadata.common.musicbrainz_trackid
-              })
-              logger.info(`Artist: ${metadata.common.albumartist}, Album: ${metadata.common.album}, Title: ${metadata.common.title}`)
-            } else {
-              console.log('Already in db')
-            }
-          } catch (err) {
-            logger.error(err)
-            return null
-          }
+          await this.processFile(pathname)
         } else if (dirent.name.includes('cover.')) { // Detect cover files and insert them into the DB on scan
-          const dirPath = path.dirname(pathname)
-          const files = await fsp.readdir(dirPath)
-          for (const file of files) {
-            if (file.endsWith('.flac')) {
-              console.log(path.resolve(dirPath, file))
-              try {
-                const metadata = await mm.parseFile(path.resolve(dirPath, file))
-                console.log(`cover is for ${metadata.common.album}`)
-                break
-              } catch (error) {
-                console.log(error)
-                break
-              }
-            }
-          }
+          this.processCover(pathname)
         }
       }
       return null
     }
-    /*     const now = new Date()
-    const isoString = now.toISOString()
-    logger.info(isoString) */
-    /*  const emitter = walk.walk('I:\\Music')
-    emitter.on('errors', function (root, nodeStatsArray, next) {
-      console.log(root)
-      console.log(nodeStatsArray)
-    })
-    emitter.on('end', function () {
-      console.log('all done')
-    })
-    emitter.on('directories', function (root, dirStatsArray, next) {
-      // dirStatsArray is an array of `stat` objects with the additional attributes
-      // * type
-      // * error
-      // * name
-      console.log(dirStatsArray)
-      console.log(root)
-      next()
-    })
-    emitter.on('file', async (filename, stat, next) => {
-      if (filename.endsWith('.flac')) {
-        const metadata = await mm.parseFile(filename)
-        logger.debug(filename)
-        if (!await this.db.models.song.findOne({ where: { title: metadata.common.title, album: metadata.common.album } })) {
-          await this.db.models.song.create({
-            title: metadata.common.title,
+    console.timeEnd('fullScan')
+  }
+
+  async startPartialScan() {
+    console.time('partialScan')
+    // if (this.scanning) return false
+    this.updateScanStatus(true)
+    const directories = await this.db.models.directory.findAll()
+    const unchanged = []
+    for (const directory of directories) {
+      const path = directory.dataValues.path
+      if (path === '/mnt/g/aa') continue
+      this.log.debug(`Checking unchanged list for dir ${path}`)
+
+      if (!unchanged.some(v => path.includes(v))) {
+        this.log.debug(`Scanning dir ${path}`)
+        // There's at least on
+        if (!fs.existsSync(path)) {
+          this.log.debug(`Deleting dir ${path}`)
+          await this.db.query(`DELETE FROM directories WHERE path like '%${path}%'`)
+          await this.db.query(`DELETE FROM songs WHERE path like '%${path}%'`)
+          await this.db.query(`DELETE FROM albums WHERE path like '%${p.resolve(path, '..', '..')}%'`)
+
+          continue
+        }
+        /*       console.log(unchanged.length)
+      path.resolve(path, '..', '..');
+      for (const dir of unchanged) {
+        if (String(path).includes(dir) ) continue  walk
+      } */
+        const mtime = directory.dataValues.mtime
+        const stats = fs.statSync(path)
+        if (stats.mtimeMs > mtime) {
+          this.walkDirectory(path)
+        } else {
+          unchanged.push(path)
+          this.log.debug(`Directory ${path} has not changed`)
+        }
+      }
+    }
+    // this.updateAlbums()
+    this.createAlbums()
+    console.timeEnd('partialScan')
+  }
+
+  async walkDirectory(path) {
+    console.time('scan')
+    await Walk.walk(path, walkFunc.bind(this))
+    console.timeEnd('scan')
+    // walkFunc must be async, or return a Promise
+    async function walkFunc(err, pathname, dirent) {
+      if (err) {
+        console.warn('fs stat error for %s: %s', pathname, err.message)
+        return null
+      }
+      if (dirent.isDirectory()) {
+        const stats = fs.statSync(path)
+        this.updateOrCreate(this.db.models.directory, { path: pathname }, { path: pathname, mtime: stats.mtimeMs })
+      }
+      if (dirent.isFile()) {
+        if (dirent.name.endsWith('.flac')) {
+          const metadata = await mm.parseFile(pathname)
+          this.updateOrCreate(this.db.models.song, { path: pathname }, {
+            path: pathname,
+            title: metadata.common.title || 'Unknown',
             disk: metadata.common.disk.no,
-            album: metadata.common.album,
-            artist: metadata.common.artist,
+            album: metadata.common.album || 'Unknown',
+            artist: metadata.common.artist || 'Unknown',
             track: metadata.common.track.no,
             codec: metadata.format.codec,
             sampleRate: metadata.format.sampleRate,
@@ -139,24 +150,197 @@ class MusicScanner extends EventEmitter {
             musicBrainzArtistId: metadata.common.musicbrainz_artistid,
             musicBrainzTrackId: metadata.common.musicbrainz_trackid
           })
-          next()
-          logger.info(`Artist: ${metadata.common.albumartist}, Album: ${metadata.common.album}, Title: ${metadata.common.title}`)
-        } else {
-          console.log('Already in db')
-        }
-      } else if (filename.includes('cover.')) { // Detect cover files and insert them into the DB on scan
-        const dirPath = path.dirname(filename)
-        const files = await fsp.readdir(dirPath)
-        for (const file of files) {
-          if (file.endsWith('.flac')) {
-            const metadata = await mm.parseFile(path.resolve(dirPath, file))
-            console.log(`cover is for ${metadata.common.album}`)
-            break
-          }
+          this.albumsToUpdate.push({
+            path: pathname,
+            title: metadata.common.title || 'Unknown',
+            disk: metadata.common.disk.no,
+            album: metadata.common.album || 'Unknown',
+            artist: metadata.common.artist || 'Unknown',
+            track: metadata.common.track.no,
+            codec: metadata.format.codec,
+            sampleRate: metadata.format.sampleRate,
+            bitsPerSample: metadata.format.bitsPerSample,
+            year: metadata.common.year,
+            label: metadata.common.label,
+            musicBrainzRecordingId: metadata.common.musicbrainz_recordingid,
+            musicBrainzArtistId: metadata.common.musicbrainz_artistid,
+            musicBrainzTrackId: metadata.common.musicbrainz_trackid
+          })
+          // this.processFile(pathname)
+        } else if (dirent.name.includes('cover.')) { // Detect cover files and insert them into the DB on scan
+          this.processCover(pathname)
         }
       }
-    })
-    this.scanning = false */
+      return null
+    }
+  }
+
+  /* async startScanning() {
+    // if (this.scanning) return false
+    this.updateScanStatus(true)
+    console.time('scan')
+    await Walk.walk('/mnt/g/aa', walkFunc.bind(this))
+    console.timeEnd('scan')
+    // walkFunc must be async, or return a Promise
+    async function walkFunc(err, pathname, dirent) {
+      if (err) {
+        console.warn('fs stat error for %s: %s', pathname, err.message)
+        return null
+      }
+      if (dirent.isDirectory()) {
+        if(pathname === '/mnt/g/aa') return true
+        const directory = await this.db.models.directory.findOne({ where: { path: pathname } })
+        const stats = fs.statSync(pathname)
+        if (!directory) {
+          //this.createDirectory(pathname, stats.mtimeMs)
+        } else {
+          console.log(pathname)
+          console.log(`DBTIME:${directory.mtime} FTIME:${stats.mtimeMs}`)
+          if (stats.mtimeMs > directory.mtime) {
+            this.updateOrCreate(this.db.models.directory, {path: pathname},{path: pathname, mtime: stats.mtimeMs} )
+            console.log('Directory changed')
+          } else {
+            console.log('Directory not changed')
+            return false
+          }
+        }
+        //await this.db.models.directory.create({name: pathname, mtime: fs.statSync(pathname).mtimeMs})
+      }
+      if (dirent.isFile()) {
+        if (dirent.name.endsWith('.flac')) {
+          this.processFile(pathname)
+        } else if (dirent.name.includes('cover.')) { // Detect cover files and insert them into the DB on scan
+          this.processCover(pathname)
+        }
+      }
+      return null
+    }
+  } */
+  /*   async processDirectory(pathname) {
+    const directory = await this.db.models.directory.findOne({ where: { path: pathname } })
+    const stats = fs.statSync(pathname)
+    if (!directory) {
+      this.createDirectory(pathname, stats.mtimeMs)
+    } else {
+      if (stats.mtimeMs > directory.mtime) {
+        this.updateOrCreate(this.db.models.directory, {path: pathname},{path: pathname, mtime: stats.mtimeMs} )
+        console.log('Directory changed')
+      } else {
+        console.log('Directory not changed')
+        return false
+      }
+    }
+  } */
+  async processFile(pathname) {
+    try {
+      const metadata = await mm.parseFile(pathname)
+      // this.processTrack(metadata, String(pathname))
+      await this.db.models.song.create({
+        path: pathname,
+        title: metadata.common.title || 'Unknown',
+        disk: metadata.common.disk.no,
+        album: metadata.common.album || 'Unknown',
+        artist: metadata.common.artist || 'Unknown',
+        track: metadata.common.track.no,
+        codec: metadata.format.codec,
+        sampleRate: metadata.format.sampleRate,
+        bitsPerSample: metadata.format.bitsPerSample,
+        year: metadata.common.year,
+        label: metadata.common.label,
+        musicBrainzRecordingId: metadata.common.musicbrainz_recordingid,
+        musicBrainzArtistId: metadata.common.musicbrainz_artistid,
+        musicBrainzTrackId: metadata.common.musicbrainz_trackid
+      })
+      this.log.info(`Artist: ${metadata.common.albumartist}, Album: ${metadata.common.album}, Title: ${metadata.common.title}`)
+    } catch (err) {
+      this.log.error(err)
+      return null
+    }
+  }
+
+  async processCover(pathname) {
+    const dirPath = p.dirname(pathname)
+    const files = await fsp.readdir(dirPath)
+    for (const file of files) {
+      if (file.endsWith('.flac')) {
+        try {
+          const metadata = await mm.parseFile(p.resolve(dirPath, file))
+          this.log.debug(`Cover is for ${metadata.common.album}`)
+          break
+        } catch (error) {
+          this.log.error(error)
+          break
+        }
+      }
+    }
+  }
+
+  async processTrack(metadata, path) {
+    await this.updateOrCreate(this.db.models.song, { path },
+      {
+        path,
+        title: metadata.common.title || 'Unknown',
+        disk: metadata.common.disk.no,
+        album: metadata.common.album || 'Unknown',
+        artist: metadata.common.artist || 'Unknown',
+        track: metadata.common.track.no,
+        codec: metadata.format.codec,
+        sampleRate: metadata.format.sampleRate,
+        bitsPerSample: metadata.format.bitsPerSample,
+        year: metadata.common.year,
+        label: metadata.common.label,
+        musicBrainzRecordingId: metadata.common.musicbrainz_recordingid,
+        musicBrainzArtistId: metadata.common.musicbrainz_artistid,
+        musicBrainzTrackId: metadata.common.musicbrainz_trackid
+      }
+    )
+  }
+
+  async createAlbums() {
+    const albums = await this.db.models.song.aggregate('album', 'DISTINCT', { plain: false })
+    this.db.models.song.findAll({
+      attributes: ['album'],
+      group: ['album']
+    }).then(async (albums) => {
+      for (const album of albums) {
+        const _album = await this.db.models.song.findOne({ where: { album: album.dataValues.album } })
+        await this.db.models.album.create({
+          path: p.resolve(_album.dataValues.path, '..', '..'),
+          name: _album.dataValues.album,
+          year: _album.dataValues.year
+        })
+      }
+    }
+    )
+  }
+
+  async updateAlbums() {
+    for (const album of this.albumsToUpdate) {
+      this.log.debug(`Updating album ${album.album}`)
+      const ppath = p.resolve(album.path, '..', '..')
+      await this.updateOrCreate(this.db.models.album, { path: ppath },
+        {
+          path: ppath,
+          name: album.album,
+          year: album.year
+        }
+      )
+    }
+
+    this.albumsToUpdate = []
+  }
+
+  async updateOrCreate (model, where, newItem) {
+    // First try to find the record
+    const foundItem = await model.findOne({ where })
+    if (!foundItem) {
+      // Item not found, create a new one
+      const item = await model.create(newItem)
+      return { item, created: true }
+    }
+    // Found an item, update it
+    const item = await model.update(newItem, { where })
+    return { item, created: false }
   }
 }
 
