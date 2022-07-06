699b9ccf219f1445cb0705d12db7ea5340884b74
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-06 11:00:19 +1000

    Typafy

--- src/plugins/musicScanner/index.ts
+++ src/plugins/musicScanner/index.ts
@@ -1,18 +1,18 @@
+import fp from 'fastify-plugin'
+import { FastifyInstance, FastifyPluginAsync } from 'fastify'
+import ScannerManager from './scannerManager'
 'use strict'
-const fastifyPlugin = require('fastify-plugin')
-const ScannerManager = require('./scannerManager')
-
-async function musicScannerPlugin(fastify, opts, done) {
+const scannerPlugin: FastifyPluginAsync = async (
+  fastify: FastifyInstance
+) => {
   // const musicScanner = new MusicScanner('./test_music', fastify)
   const log = fastify.log.child({ module: 'Scanner' })
   log.debug('Attaching Muisc Scanner')
 
   const manager = new ScannerManager(log)
-  manager.startChild(log)
+  manager.startChild()
   fastify.decorate('scannerManager', manager)
   log.info('Attached Muisc Scanner')
-
-  done()
 }
 
-module.exports = fastifyPlugin(musicScannerPlugin)
+export default fp(scannerPlugin)
