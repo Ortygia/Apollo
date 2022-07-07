e0e11e29de4a0a5259fb30099baa89f4a66d1536
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-08 09:13:01 +1000

    Cleanup comments

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,21 +3,21 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 async function indexRoutes(fastify: FastifyInstance) {
   /*  fastify.get('/:artistName', async (req, reply) => {
-    const searchArtist = fastify.mapi.searchArtist(decodeURI(req.params.artistName))
-    const artistId = (await searchArtist).artists[0].id
-    const getArtist = await fastify.mapi.getArtist(artistId, ['url-rels'])
-    const length = await fastify.acache.store.length()
-    reply.status(200).send(await fastify.artist.getArtistInfo(getArtist, artistId))
-  })
+      const searchArtist = fastify.mapi.searchArtist(decodeURI(req.params.artistName))
+      const artistId = (await searchArtist).artists[0].id
+      const getArtist = await fastify.mapi.getArtist(artistId, ['url-rels'])
+      const length = await fastify.acache.store.length()
+      reply.status(200).send(await fastify.artist.getArtistInfo(getArtist, artistId))
+    })
 
-  fastify.get('/', async (req, reply) => {
-    fastify.scannerManager.startScan()
-    reply.status(200).send(fastify.scannerManager.isScanning)
-  }) */
+    fastify.get('/', async (req, reply) => {
+      fastify.scannerManager.startScan()
+      reply.status(200).send(fastify.scannerManager.isScanning)
+    }) */
   fastify.get('/banner/:artistId', async (req: FastifyRequest<{
-    Params: {
-        artistId: string,
-    };
+        Params: {
+            artistId: string,
+        };
     }>,
   reply: FastifyReply) => {
     const searchArtist = await fastify.artist.getFanArtArtistBanner(req.params.artistId)
