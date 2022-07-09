c9999994570cfd303ff754f50bb3217e767ce814
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-10 01:54:28 +1000

    Add test routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,10 @@
 'use strict'
 
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { createReadStream, statSync } from 'fs'
+import { Album } from '../../models/album'
 import { Song } from '../../models/song'
+import computeRange from '../../utils'
 async function indexRoutes(fastify: FastifyInstance) {
   /*  fastify.get('/:artistName', async (req, reply) => {
       const searchArtist = fastify.mapi.searchArtist(decodeURI(req.params.artistName))
@@ -15,6 +18,32 @@ async function indexRoutes(fastify: FastifyInstance) {
       fastify.scannerManager.startScan()
       reply.status(200).send(fastify.scannerManager.isScanning)
     }) */
+  fastify.get('/getAlbumList', async (req: FastifyRequest<{
+      Params: {
+          artistId: string,
+      };
+  }>,
+  reply: FastifyReply) => {
+    const albums = await Album.findAll({
+      raw: true,
+      nest: true
+    })
+    reply.status(200).send(albums)
+  })
+  fastify.get('/getArtistAlbum', async (req: FastifyRequest<{
+    Querystring: {
+        id: string,
+    };
+}>,
+  reply: FastifyReply) => {
+    const album = await Album.findOne({
+      where: { id: req.query.id },
+
+      include: { all: true }
+    })
+
+    reply.status(200).send(album)
+  })
   fastify.get('/banner/:artistId', async (req: FastifyRequest<{
         Params: {
             artistId: string,
@@ -23,6 +52,35 @@ async function indexRoutes(fastify: FastifyInstance) {
   reply: FastifyReply) => {
     const url = await fastify.artist.getFanArtArtistBanner(req.params.artistId)
     reply.status(200).send(url)
+  })
+  fastify.get('/stream/:songId', async (req: FastifyRequest<{
+      Params: {
+        songId: string,
+      };
+    }>,
+  reply: FastifyReply) => {
+    const song = await Song.findOne({ where: { id: req.params.songId } })
+    if (song) {
+      const stat = statSync(song.path)
+      const range = computeRange(stat.size, req.headers.range)
+      const stream = createReadStream(song.path, range)
+
+      return reply
+        .status(206)
+        .headers({
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          Pragma: 'no-cache',
+          Expires: 0,
+          connection: 'keep-alive',
+          'Content-Type': 'audio/flac',
+          'Accept-Ranges': 'bytes',
+          'Content-Length': range.end - range.start + 1,
+          'Content-Range': `bytes ${range.start}-${range.end}/${stat.size}`
+        })
+        .send(stream)
+    } else {
+      return reply.status(404).send('Song not found')
+    }
   })
   fastify.get('/full', async (req, reply) => {
     fastify.scannerManager.startScan()
