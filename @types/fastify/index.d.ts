9f0911a03bd82e990d2874396c0261a52e0a11ea
112RG <112RG@users.noreply.github.com>
2022-07-13 04:45:22 +1000

    Add db to types

--- @types/fastify/index.d.ts
+++ @types/fastify/index.d.ts
@@ -1,8 +1,10 @@
+import { Sequelize } from 'sequelize/types'
 import ArtistService from '../../src/plugins/artist/service'
 import ScannerManager from '../../src/plugins/musicScanner/scannerManager'
 declare module 'fastify' {
   export interface FastifyInstance {
     scannerManager: ScannerManager
     artist: ArtistService
+    db: Sequelize
   }
 }
