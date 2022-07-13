716dcc699266016c040f574e9ac5c29012c6abf2
112RG <112RG@users.noreply.github.com>
2022-07-14 00:25:56 +1000

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 'use strict'
 import { buildFastify, start } from './server/app'
-// const { fork } = require('child_process')
-
 async function run() {
-  // const scannerProccess = startScannerProccess()
   await start(await buildFastify())
 }
 
-/* function startScannerProccess() {
-  const child = fork('./src/scanner/index.js')
-  return child
-} */
 run().catch((error) => console.error(error))
